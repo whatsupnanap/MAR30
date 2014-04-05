babf023d7fa1cfe26255a3a63d080f67759d1ad1
whatsupnanap <nanat@fullsail.edu>
2014-04-04 23:30:18 -0400

    lab-3 11:29pm

--- april2lecture/Resources/app.js
+++ april2lecture/Resources/app.js
@@ -22,6 +22,34 @@ var appName = Ti.UI.createLabel({
 	font:{fontSize: "20dp", fontFamily:"Helvetica"}
 });
 
+var rollBtn = Ti.UI.createView({
+	backgroundColor:"#333",
+	width: 250,
+	height:60
+});
+
+var rollBtnLabel = Ti.UI.createLabel({
+	text:"Roll the dice!",
+	color:"#FFF",
+	font:{fontSize:"14dp", fontFamily:"Helvetica"}
+});
+
+var getRoll = function(){
+	var dice = [1,2,3,4,5,6];
+	var rolls = [dice, dice];
+	var results = [];
+
+for(var i=0, j=rolls.length;i<j;i++){
+	var theRoll = Math.floor(Math.random()*dice.length)+1;
+	console.log(theRoll);
+	results.push(theRoll);
+}
+	alert("final result is " + results);
+};
+
+rollBtn.addEventListener("click",getRoll);
+rollBtn.add(rollBtnLabel);
+mainWindow.add(rollBtn);
 header.add(appName);
 mainWindow.add(header);
 //windows open
